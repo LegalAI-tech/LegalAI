9b39ba0c26b1943635137fdbb94c2f1b16a278d9
Anubrata Guin <anu.guin.01@gmail.com>
2025-10-03 18:33:02 +0530

    removed duplicate

--- Frontend/src/app/home/loading.tsx
+++
@@ -1,9 +0,0 @@
-import BounceLoader from "@/components/ui/bounce-loader";
-
-export default function Loading() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-background">
-      <BounceLoader />
-    </div>
-  );
-}
